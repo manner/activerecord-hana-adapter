b773b9df688ce5de341805a633d9d4570e27b993
Moritz Manner <moritz.manner@student.hpi.de>
2018-04-04 13:35:39 +0200

    Fix for newer ruby versions

--- lib/active_record/connection_adapters/hana_adapter.rb
+++ lib/active_record/connection_adapters/hana_adapter.rb
@@ -18,25 +18,25 @@ require 'active_record/connection_adapters/hana/core_ext/relation/finder_methods
 module ActiveRecord
 
   class Base
-    
+
     def self.hana_connection(config)
       config = config.symbolize_keys
-      
+
       raise ArgumentError, 'Missing :dsn configuration.' unless config.has_key?(:dsn)
       raise ArgumentError, 'Missing :username configuration.' unless config.has_key?(:username)
-      raise ArgumentError, 'Missing :password configuration.' unless config.has_key?(:password) 
-      raise ArgumentError, 'Missing :database configuration.' unless config.has_key?(:database) 
-        
-      ConnectionAdapters::HanaAdapter.new(nil, logger, nil, config) 
+      raise ArgumentError, 'Missing :password configuration.' unless config.has_key?(:password)
+      raise ArgumentError, 'Missing :database configuration.' unless config.has_key?(:database)
+
+      ConnectionAdapters::HanaAdapter.new(nil, logger, nil, config)
     end
-  
+
   end
 
   module ConnectionAdapters
 
     class HanaColumn < Column
 
-    
+
     end #class HanaColumn
 
     class HanaAdapter < AbstractAdapter
@@ -44,32 +44,32 @@ module ActiveRecord
       include Hana::DatabaseStatements
       include Hana::SchemaStatements
       include Hana::Quoting
-    
+
       def initialize(connection, logger, pool, config)
-        super(connection, logger, pool)
-              
+        super(connection, logger, config)
+
         @schema_cache = Hana::SchemaCache.new self
         @visitor = Arel::Visitors::Hana.new self
 
-        @connection_options = config    
-        
+        @connection_options = config
+
         connect
         setup_schema
-        
+
       end
-    
+
       def connect
-        @connection =  
+        @connection =
           if @connection_options[:dsn].include?(';')
             driver = ODBC::Driver.new.tap do |d|
               d.name = @connection_options[:dsn_name] || 'Driver1'
               d.attrs = @connection_options[:dsn].split(';').map{ |atr| atr.split('=') }.reject{ |kv| kv.size != 2 }.inject({}){ |h,kv| k,v = kv ; h[k] = v ; h }
              end
              ODBC::Database.new.drvconnect(driver)
-                        
+
           else
             ODBC.connect @connection_options[:dsn], @connection_options[:username], @connection_options[:password]
-          end.tap do |c|  
+          end.tap do |c|
             begin
               c.use_time = true
               c.use_utc = ActiveRecord::Base.default_timezone == :utc
@@ -87,31 +87,31 @@ module ActiveRecord
         set_schema desired_schema
       end
 
-      # === Abstract Adapter ========================================== #     
-      
+      # === Abstract Adapter ========================================== #
+
       # Returns the human-readable name of the adapter.
       def adapter_name
         'Hana'
-      end     
+      end
 
       def clear_cache!
         @statements.clear
       end
-      
+
       # Should primary key values be selected from their corresponding
       # sequence before the insert statement? If true, next_sequence_value
       # is called before each insert to set the record's primary key.
       # This is false for all adapters but Firebird.
       #
       # This value is set to false, because the adapter auto-generate a
-      # sequence for each table. If this value would be set to true the 
-      # user has to spezify a sequence for each table in the migration. 
+      # sequence for each table. If this value would be set to true the
+      # user has to spezify a sequence for each table in the migration.
       def prefetch_primary_key?(table_name = nil)
         true
       end
 
       def requires_reloading?
-        false     
+        false
       end
 
       def supports_bulk_alter?
@@ -136,7 +136,7 @@ module ActiveRecord
 
       def supports_migrations?
         true
-      end   
+      end
 
       def supports_primary_key?
         true
@@ -153,7 +153,7 @@ module ActiveRecord
       def primary_key(table_name)
         row = select_values "SELECT COLUMN_NAME FROM CONSTRAINTS WHERE SCHEMA_NAME=\'#{@connection_options[:database]}\' AND TABLE_NAME=\'#{table_name}\' AND IS_PRIMARY_KEY=\'TRUE\'"
         row && row.first
-      end 
+      end
 
       # === Abstract Adapter (Connection Management) ================== #
 
--- lib/arel/visitors/hana.rb
+++ lib/arel/visitors/hana.rb
@@ -3,9 +3,7 @@ require 'arel'
 module Arel
   module Visitors
     class Hana < Arel::Visitors::ToSql
-    
+
     end
   end
 end
-
-Arel::Visitors::VISITORS['hana'] = Arel::Visitors::Hana
